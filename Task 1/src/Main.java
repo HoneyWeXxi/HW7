69a9fe12fa270add89f7421e1574d97de72a43d9
WeXxi <wexxichannel@gmail.com>
2024-06-18 18:30:39 +0700

    Задача решена

--- Task 1/src/Main.java
+++ Task 1/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         int toadEyesCount = 0;          //Жабьи глаза
@@ -6,6 +8,12 @@ public class Main {
         int dumplingsCount = 0;         //Пельмени(а почему, собственно нет? xD )
 
         //todo реализовать ввод пользователем кол-ва ингредиентов.
+        System.out.println("Введите кол-во ингредиентов: ");
+        toadEyesCount = new Scanner(System.in).nextInt();
+        ghoulTearsCount = new Scanner(System.in).nextInt();
+        ravenBonesCount = new Scanner(System.in).nextInt();
+        dumplingsCount = new Scanner(System.in).nextInt();
+
 
         //Рецепты Эликсиров:
         // 1. 3 Жабьих глаза + 1 кость ворона - Эликсир зоркости
@@ -14,5 +22,32 @@ public class Main {
         // 4. 5 слез вурдалака + 10 пельменей + 4 жабьих глаза + 3 кости ворона - Запретный эликсир
 
         //todo дописать логику программы сюда
+        int elexirCount = 0;
+        while () {
+            if (toadEyesCount >= 3 && ravenBonesCount >= 1) {
+                elexirCount++;
+                toadEyesCount -= 3;
+                ravenBonesCount -= 1;
+            }
+            if (ravenBonesCount >= 2 && dumplingsCount >= 4) {
+                elexirCount++;
+                ravenBonesCount -= 2;
+                dumplingsCount -= 4;
+            }
+            if (ghoulTearsCount >= 7 && dumplingsCount >= 1 && toadEyesCount >= 2) {
+                elexirCount++;
+                ghoulTearsCount -= 7;
+                dumplingsCount -= 1;
+                toadEyesCount -= 2;
+            }
+            if (ghoulTearsCount >= 5 && dumplingsCount >= 10 && toadEyesCount >= 4 && ravenBonesCount >= 3) {
+                elexirCount++;
+                ghoulTearsCount -= 5;
+                ravenBonesCount -= 3;
+                dumplingsCount -= 10;
+                toadEyesCount -= 4;
+            }
+        }
+        System.out.println(elexirCount);
     }
 }
