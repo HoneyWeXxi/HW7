45b1938afcebbf39788bd7e6f23fb44773c578e7
WeXxi <wexxichannel@gmail.com>
2024-06-19 17:23:02 +0700

    Оптимальное решение

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         //todo Тут увольняем айтишников
-        for (int i = 1; i <= 100 ; i++) {
-            if (i % 2 == 0){
+        for (int i = 2; i <= 100 ; i += 2) {
                 System.out.println("Уволен айтишник с id " + i);
-            }
         }
     }
 }
